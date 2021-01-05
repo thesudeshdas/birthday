920ad613ea010d1bead48b92c525200313c43030
thesudeshdas <sudeshkumardas7@gmail.com>
2021-01-05 11:54:03 +0530

    fix: hashrouter added

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import "./style.css";
-import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
+import { HashRouter as Router, Switch, Route } from "react-router-dom";
 
 import {
   NAVIGATION,
